4b8970d345b4dfdcf900bc2f9b4fafbf9b7c63d8
lu13le <raicevic12345@gmail.com>
2021-05-04 17:47:13 +0200

    Create Action changes

--- RestaurantManagement/Areas/Admin/Controllers/MenuItemController.cs
+++ RestaurantManagement/Areas/Admin/Controllers/MenuItemController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RestaurantManagement.Data;
 using RestaurantManagement.Models.ViewModels;
+using RestaurantManagement.Utility;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,16 +35,61 @@ namespace RestaurantManagement.Areas.Admin.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var menuItems = await _db.MenuItems.Include(m=>m.Category).Include(m=>m.Subcategory).ToListAsync();
-
+            var menuItems = await _db.MenuItems.Include(m => m.Category).Include(m => m.Subcategory).ToListAsync();
             return View(menuItems);
         }
 
-        //GET - Cretae
-
+        //GET - CREATE
         public IActionResult Create()
         {
             return View(MenuItemVM);
         }
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePOST()
+        {
+            MenuItemVM.MenuItem.SubcategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _db.MenuItems.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+
+            //Work on the image saving section
+
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = await _db.MenuItems.FindAsync(MenuItemVM.MenuItem.Id);
+
+            if (files.Count > 0)
+            {
+                //files has been uploaded
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filesStream);
+                }
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+            else
+            {
+                //no file was uploaded, so use default
+                var uploads = Path.Combine(webRootPath, @"images\" + SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
+
---
+++ RestaurantManagement/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Utility
+{
+    public static class SD
+    {
+        public const string DefaultFoodImage = "logo.jpg";
+    }
+}
