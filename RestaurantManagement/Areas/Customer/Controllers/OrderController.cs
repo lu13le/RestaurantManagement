9feef9d5ac589deb86082b0e92e26ef106b454e6
lu13le <raicevic12345@gmail.com>
2021-05-21 14:22:58 +0200

    Working on order preview page

---
+++ RestaurantManagement/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantManagement.Data;
+using RestaurantManagement.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Confirm(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            OrderDetailsViewModel orderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = await _db.OrderHeaders.Include(o => o.ApplicationUser).FirstOrDefaultAsync(o => o.Id == id && o.UserId == claim.Value),
+                OrderDetails = await _db.OrderDetails.Where(o => o.OrderId == id).ToListAsync()
+            };
+            return View(orderDetailsViewModel); 
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
