d16415a204e52a142efb458454699e78da2fa860
Luka Raicevic <raicevic12345@gmail.com>
2022-11-18 14:20:22 +0100

    Adding files

---
+++ RestaurantManagement/Data/Migrations/20211219191751_freshStart.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantManagement.Data.Migrations
+{
+    public partial class freshStart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
